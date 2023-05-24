43a3ebe525d954477d63a44f7ae8b036a7622255
Santiago Corco Schnake <44772527+hibaba2@users.noreply.github.com>
2023-05-24 03:14:05 -0400

    v1
    v1

--- reconocedoremociones.py
+++ reconocedoremociones.py
@@ -1,46 +1,68 @@
 import cv2
-from deepface import DeepFace
 import numpy as np
+from deepface import DeepFace
 
 # Inicializar la cámara
 cap = cv2.VideoCapture(0)
+
+# Cargar la imagen del banner
+image_path = 'src/banner.png'  # Reemplaza con la ruta correcta de la imagen
+image = cv2.imread(image_path)
+
+# Obtener las dimensiones originales del banner
+banner_height, banner_width = image.shape[:2]
+
+# Diccionario de traducción de emociones
+dicOriginal = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
+dicEspanol = ['Enojo >:(   ', 'Disgusto :S ', 'Miedo D:   ', 'Felicidad :D ', 'Tristeza :( ', 'Sorpresa :o', 'Neutro :|  ']
+dd = dict(zip(dicOriginal, dicEspanol))
 
 while True:
     # Leer el cuadro de la cámara
     ret, frame = cap.read()
 
-    # Realizar el reconocimiento facial en el cuadro
+    # Obtener las dimensiones del cuadro de video
+    frame_height, frame_width = frame.shape[:2]
+
+    # Ajustar la imagen a las dimensiones originales del banner
+    if image is not None:
+        image_resized = cv2.resize(image, (frame_width, banner_height))
+
+        # Combinar el banner con el cuadro de video
+        combined_frame = cv2.vconcat([image_resized, frame])
+    else:
+        combined_frame = frame
+
     try:
+        # Realizar el reconocimiento facial en el cuadro
         results = DeepFace.analyze(frame, actions=['emotion'])
 
         # Extraer las emociones del primer resultado
         emotions = results[0]['emotion']
 
-        # Mostrar las emociones en el cuadro
-        dicOriginal=['angry','disgust','fear','happy','sad','surprise','neutral']
-        dicEspanol=['Enojo    ',
-                    'Disgusto ',
-                    'Miedo    ',
-                    'Felicidad',
-                    'Tristeza ',
-                    'Sorpresa ',
-                    'Neutro   ']
-        dd=dict(zip(dicOriginal,dicEspanol))
-        cv2.putText(frame,"UTEM", (600, 30 + 20),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 255), 2)
-        for idx, (emotion, score) in enumerate(emotions.items()):
-            if score>30:
-                cv2.putText(frame, "%4s:%1.2f"%(dd[emotion],score), (10, 30 + idx * 20),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
-            else:
-                cv2.putText(frame, "%4s:%1.2f"%(dd[emotion],score), (10, 30 + idx * 20),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-            
-            #cv2.putText(frame, f"{emotion}: {score}", (10, 30 + idx * 20),
-             #           cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        # Obtener la emoción con el mayor valor
+        max_emotion = max(emotions, key=emotions.get)
 
-        # Mostrar el cuadro con las emociones
-        cv2.imshow('Facial Emotion Recognition', frame)
+        # Crear el texto de la emoción
+        emotion_text = dd[max_emotion]
+
+        # Obtener el tamaño del texto
+        text_size, _ = cv2.getTextSize(emotion_text, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)
+
+        # Calcular la posición del texto centrado en la parte inferior
+        text_x = int((frame_width - text_size[0]) / 2)
+        text_y = banner_height + frame_height + 30
+
+        # Crear una imagen en blanco con el tamaño adecuado para mostrar la emoción
+        emotion_image = np.zeros((banner_height, frame_width, 3), dtype=np.uint8)
+        cv2.putText(emotion_image, emotion_text, (text_x, int(banner_height / 2)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)  # Color azul (BGR)
+
+        # Combinar el cuadro de video con el banner y la emoción
+        combined_frame = cv2.vconcat([combined_frame, emotion_image])
+
+        # Mostrar el cuadro combinado con la emoción predominante
+        cv2.imshow('Facial Emotion Recognition', combined_frame)
     except:
         print('No detecta rostro')
 
